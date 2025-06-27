1eccb58e9fe32a39e9484a34b11c597f66d4bf1d
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-06-26 23:15:13 -0500

    feat: :sparkles: Primera funcion creada

--- dql_funciones.sql
+++ dql_funciones.sql
@@ -1,1 +1,19 @@
 USE BancoCL;
+
+-- 1era funcion: calcular el promedio de valor de cuotas por tipo de tarjeta
+DELIMITER $$
+
+CREATE FUNCTION calcular_promedio_cuota(tipo_id INT)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+  DECLARE promedio DECIMAL(10,2);
+  SELECT AVG(cm.monto_final)
+  INTO promedio
+  FROM cuotas_manejo cm
+  JOIN tarjetas t ON cm.tarjeta_id = t.tarjeta_id
+  WHERE t.tipo_tarjeta_id = tipo_id;
+  RETURN promedio;
+END$$
+
+DELIMITER ;
