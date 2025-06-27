a830a56b27b350e7660d125a5aa62043c562797d
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-06-26 23:20:02 -0500

    feat: :sparkles: Creacion 2da funcion

--- dql_funciones.sql
+++ dql_funciones.sql
@@ -17,3 +17,25 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- 2da funcion: calcular el promedio de valor de cuotas por tipo de tarjeta
+DELIMITER $$
+
+CREATE FUNCTION total_pagado_cliente(cliente INT)
+RETURNS DECIMAL(12,2)
+DETERMINISTIC
+BEGIN
+  DECLARE total DECIMAL(12,2);
+  SELECT SUM(hp.monto_pagado)
+  INTO total
+  FROM historial_pagos hp
+  JOIN cuotas_manejo cm ON hp.cuota_id = cm.cuota_id
+  JOIN tarjetas t ON cm.tarjeta_id = t.tarjeta_id
+  WHERE t.cliente_id = cliente AND hp.estado_transaccion = 'Exitoso';
+  RETURN IFNULL(total, 0);
+END$$
+
+DELIMITER ;
+
+
+
